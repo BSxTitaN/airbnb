655cc1ae086d43bd93b4f23a3c0002455081709a
BSxTitaN <hsp22903@gmail.com>
2021-08-11 22:12:29 +0530

    first commit

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -8,7 +8,7 @@ import {
 
 function Header() {
   return (
-    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-15">
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/* Left */}
       <div className="relative flex items-center h-10 cursor-pointer my-auto">
         <Image
