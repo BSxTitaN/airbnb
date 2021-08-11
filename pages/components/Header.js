f8c52c0122d7402f32b128eb3513e8a529db656f
BSxTitaN <hsp22903@gmail.com>
2021-08-11 22:09:18 +0530

    first commit

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -8,7 +8,7 @@ import {
 
 function Header() {
   return (
-    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-15">
       {/* Left */}
       <div className="relative flex items-center h-10 cursor-pointer my-auto">
         <Image
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import Footer from "./components/Footer";
 
 export default function Home({ exploreData, cardsData }) {
   return (
-    <div className="md:min-w-400px xl:min-w-600px">
+    <div className="">
       <Head>
         <title>Airbnb - Home</title>
         <link rel="icon" href="/favicon.ico" />
