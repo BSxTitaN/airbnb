8ee046de540a4040e4c35851c0acfb94022ecdff
BSxTitaN <hsp22903@gmail.com>
2021-08-11 21:58:12 +0530

    first commit

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import Footer from "./components/Footer";
 
 export default function Home({ exploreData, cardsData }) {
   return (
-    <div className="md:min-w-400px">
+    <div className="md:min-w-600px">
       <Head>
         <title>Airbnb - Home</title>
         <link rel="icon" href="/favicon.ico" />
