32bd70b52aea7e0ae3b460d23afad2b5ae5e7a39
BSxTitaN <hsp22903@gmail.com>
2021-08-14 16:54:44 +0530

    first commit

--- pages/components/Banner.js
+++ pages/components/Banner.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function Banner() {
   return (
-    <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
+    <div className="relative  h-[500px] sm:h-[550px] lg:h-[700px] xl:h-[700px] 2xl:h-[800px] overflow-hidden">
       <Image
         src="https://links.papareact.com/0fm"
         layout="fill"
