241e0fbbc2b9eff12dd464b2aea960e0e45a0e1d
BSxTitaN <hsp22903@gmail.com>
2021-08-15 18:17:07 +0530

    first commit

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -10,6 +10,7 @@ import { useState } from "react";
 import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import { DateRangePicker } from "react-date-range";
+import { DateRange } from "react-date-range";
 import Fade from "react-reveal/Fade";
 
 function Header() {
@@ -17,23 +18,30 @@ function Header() {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   const [noOfGuests, setNoOfGuests] = useState(1);
+  const [state, setState] = useState([
+    {
+      startDate: new Date(),
+      endDate: null,
+      key: "selection",
+    },
+  ]);
 
   //console.log(searchInput);
 
-  const handleSelect = (ranges) => {
-    setStartDate(ranges.selection.startDate);
-    setEndDate(ranges.selection.endDate);
-  };
+  // const handleSelect = (ranges) => {
+  //   setStartDate(ranges.selection.startDate);
+  //   setEndDate(ranges.selection.endDate);
+  // };
 
   const resetInput = () => {
     setSearchInput("");
   };
 
-  const selectionRange = {
-    startDate: startDate,
-    endDate: endDate,
-    key: "selection",
-  };
+  // const selectionRange = {
+  //   startDate: startDate,
+  //   endDate: endDate,
+  //   key: "selection",
+  // };
 
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
@@ -69,33 +77,40 @@ function Header() {
         </div>
       </div>
       {searchInput && (
-          <div className="flex flex-col col-span-3 mx-auto">
-            <DateRangePicker
-              ranges={[selectionRange]}
-              minDate={new Date()}
-              rangeColors={["#FD5B61"]}
-              onChange={handleSelect}
+        <div className="flex flex-col col-span-3 mx-auto pt-3">
+          {/* <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5B61"]}
+            onChange={handleSelect}
+          /> */}
+          <DateRange
+            editableDateInputs={true}
+            onChange={(item) => setState([item.selection])}
+            moveRangeOnFirstSelection={false}
+            ranges={state}
+            rangeColors={["#FD5B61"]}
+          />
+          <div className="flex items-center  border-b mb-4">
+            <h2 className="text-2xl flex-grow font-semibold">
+              Number of Guests
+            </h2>
+            <UsersIcon className="h-5" />
+            <input
+              value={noOfGuests}
+              onChange={(e) => setNoOfGuests(e.target.value)}
+              type="number"
+              min={1}
+              className="w-12 pl-2 text-lg outline-none text-red-400"
             />
-            <div className="flex items-center  border-b mb-4">
-              <h2 className="text-2xl flex-grow font-semibold">
-                Number of Guests
-              </h2>
-              <UsersIcon className="h-5" />
-              <input
-                value={noOfGuests}
-                onChange={(e) => setNoOfGuests(e.target.value)}
-                type="number"
-                min={1}
-                className="w-12 pl-2 text-lg outline-none text-red-400"
-              />
-            </div>
-            <div className="flex">
-              <button onClick={resetInput} className="flex-grow text-gray-500">
-                Cancel
-              </button>
-              <button className="flex-grow text-red-400">Search</button>
-            </div>
           </div>
+          <div className="flex">
+            <button onClick={resetInput} className="flex-grow text-gray-500">
+              Cancel
+            </button>
+            <button className="flex-grow text-red-400">Search</button>
+          </div>
+        </div>
       )}
     </header>
   );
